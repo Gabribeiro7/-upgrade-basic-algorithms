969b30fce2b3ea6693b6bae3d6f87d29c920ea39
Gabribeiro7 <gabriel.cardoso@bootcamp-upgrade.com>
2023-01-03 19:00:01 +0100

    Iteración 1

---
+++ basic-Algorithms.js
@@ -0,0 +1,41 @@
+//! Iteración 1
+
+//? Iteración 1.1 Crea una variable llamada myFavoriteHero, asigna el valor Hulk a ella.
+    let myFaviriteHero = "Hulk";
+//? Iteración 1.2 Crea una variable llamada x, asigna el valor 50 a ella.
+    let x = 50;
+//? Iteración 1.3 Crea una variable llamada 'h' con el valor 5 y otra 'y' con el valor 10.
+    let h = 5 ;
+    let y = 10;
+//? Iteración 1.4 Crea una otra variable 'z' y asignale el valor de 'h' + 'y'.
+    let z = h + y;
+
+
+//! Iteración 2
+
+//? Iteración 2.1 
+//? Iteración 2.2  
+//? Iteración 2.3
+//? Iteración 2.4
+
+//! Iteración 3
+//? Iteración 3.1
+//? Iteración 3.2
+//? Iteración 3.3
+//? Iteración 3.4
+//? Iteración 3.5
+
+//! Iteración 4
+//? Iteración 4.1
+//? Iteración 4.2
+//? Iteración 4.3
+//? Iteración 4.4
+//? Iteración 4.5
+//? Iteración 4.6
+
+//! Iteración 5
+
+//! Iteración 6
+//? Iteración 6.1
+//? Iteración 6.2
+//? Iteración 6.3
