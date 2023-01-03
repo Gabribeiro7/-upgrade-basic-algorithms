fad54e1bfc4bb6c1ef9250b2556a55c54cc72c6d
Gabribeiro7 <gabriel.cardoso@bootcamp-upgrade.com>
2023-01-03 19:42:06 +0100

    Iteración 4

--- basic-Algorithms.js
+++ basic-Algorithms.js
@@ -77,13 +77,39 @@ console.log ("a es igual a " + a);
 
     let d = b * c;
     console.log("d es igual a " + d);
+
 //! Iteración 4
-//? Iteración 4.1
-//? Iteración 4.2
-//? Iteración 4.3
-//? Iteración 4.4
-//? Iteración 4.5
-//? Iteración 4.6
+
+//? Iteración 4.1 Consigue el valor "HULK" del array de avengers y muestralo por consola.
+    const avengers = ["HULK", "SPIDERMAN", "BLACK PANTHER"];
+    console.log(avengers[0]);
+
+//? Iteración 4.2 Cambia el primer elemento de avengers a "IRONMAN"
+
+    avengers.splice( 0, 1, "IRONMAN" );
+    console.log(avengers);
+
+//? Iteración 4.3 Alert numero de elementos en el array usando la propiedad correcta de Array.
+
+    console.log(avengers.length);
+
+//? Iteración 4.4 Añade 2 elementos al array: "Morty" y "Summer". 
+//? Muestra en consola el último personaje del array
+    const rickAndMortyCharacters = ["Rick", "Beth", "Jerry"];
+    rickAndMortyCharacters.push("Morty", "Summer");
+    console.log(rickAndMortyCharacters[rickAndMortyCharacters.length -1]);
+
+//? Iteración 4.5 Elimina el último elemento del array y muestra el primero y el último por consola.
+
+    rickAndMortyCharacters.pop();
+    console.log(rickAndMortyCharacters[0] + " y " + rickAndMortyCharacters[rickAndMortyCharacters.length -1]);
+
+//? Iteración 4.6 Elimina el segundo elemento del array y muestra el array por consola.
+
+    rickAndMortyCharacters.splice(1 ,1 );
+    console.log(rickAndMortyCharacters);
+
+    
 
 //! Iteración 5
 
